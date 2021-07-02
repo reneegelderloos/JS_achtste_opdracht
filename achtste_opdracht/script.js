ac47ba7202bb4a04b84684515f7454e3fc198805
reneegelderloos <84631679+reneegelderloos@users.noreply.github.com>
2021-07-02 20:50:45 +0200

    Add files via upload

---
+++ achtste_opdracht/script.js
@@ -0,0 +1,30 @@
+//checking if a number is bigger than 100
+
+
+
+const numberCheck = function (number1) {
+    if (number1 > 100) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+numberCheck(300);
+
+
+
+/*
+We're going to write a function that checks if a number is bigger than 100.
+
+create a function that has one parameter (argument)
+think of good names for the function and the parameter
+inside the function check the number
+if the number is bigger than 100 use the return keyword to return true
+if the number is 100 or smaller use the return keyword to return false
+now we're going to check if the function works as we intended
+call the function with different numbers
+use console.log to display the return value of the function
+the end result of this exercise should not use console.log inside of the function
+Now think about the categories of functions we discussed: functions that do something and functions that produce something. What kind of function is this?
+*/
